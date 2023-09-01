2c906f2510724c3dcff84a22743a21d76349029f
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-09-01 21:40:02 +0500

    test

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -3,12 +3,13 @@
 import React from 'react'
 
 type Props ={
- children: React.ReactNode;
+ children: React.ReactNode
+
 }
 
 function Card({children}:Props) {
   return (
-    <div className="bg-white p-2 shadow-md shadow-gray-300 mb-4 rounded-md ">{children}</div>
+    <div className="bg-white p-2 shadow-md shadow-gray-300 mb-4 rounded-sm ">{children}</div>
   )
 }
 
